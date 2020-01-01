78bc31f028f9e083318ee15e70b54628faa263a8
Veer Bawa <vbawa89@gmail.com>
2019-12-31 17:11:49 -0800

    Added i3_focus_window.sh

---
+++ i3_focus_window.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Focuses the window whose title is a superset of the first arugment to the script.
+## Multiple matches picks the first one found (no order guaranteed)
+
+# TODO: Handle multiple matches better.
+
+if [ "$#" -ne 1 ]
+then
+    echo "Wrong number of arguments"
+    echo "Usage: i3_focus_window.sh searchstring"
+    exit 1
+fi
+
+i3-msg [title=\"\(?i\)$1\"] focus
