afd75acdd1732dd556e44866692a8eab73742257
Veer Bawa <vbawa89@gmail.com>
2023-08-27 18:08:53 -0400

    Remove debug logging

--- mv_toplevel_ws.py
+++ mv_toplevel_ws.py
@@ -23,18 +23,12 @@ def main(ws_name: int):
     This means that 1 will match "1 foo" but not "10 bar"
     """
     conn = i3ipc.Connection()
-    with open("/tmp/mylog", "a") as f:
-        f.write("wses")
-        f.flush()
     wses = [ws for ws in conn.get_tree().workspaces() if ws.name == ws_name]
     assert len(wses) < 2, "Could not unambiguously identify workspace"
     # Get top-level container of the first matching workspace
     tlcs = _get_toplevel_containers(wses[0]) if len(wses) else []
 
     if len(wses) and len(tlcs) == 1:
-        with open("/tmp/mylog", "a") as f:
-            f.write("got TLC")
-            f.flush()
         # We have an unambiguous workspace and TLC
         conn.command(f"[con_id={tlcs[0].id}] mark __target")
         conn.command(f"move container to mark __target")
@@ -43,20 +37,13 @@ def main(ws_name: int):
         # There was either no matching ws in the tree, which means it's empty
         # or we have multiple TLCs
         # Fall back to regular command
-        with open("/tmp/mylog", "a") as f:
-            f.write(f"move container to workspace number {ws_name}")
-            f.flush()
         conn.command(f"move container to workspace number {ws_name}")
 
 if __name__ == "__main__":
-    with open("/tmp/mylog", "w") as f:
-        f.write("log")
-        f.write(str(sys.argv))
-        f.flush()
     assert len(sys.argv) == 2
     try:
         main(sys.argv[1])
     except Exception as e:
-        with open("/tmp/mylog", "a") as f:
+        with open("/tmp/mv_toplevel_ws_faillog", "w") as f:
             f.write(str(e))
             f.flush()
